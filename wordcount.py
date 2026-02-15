9056d908c0916e30e567fe890d071a9c2de0e0ae
srushtee <srushteepatil1212@gmail.com>
2026-02-15 12:59:33 +0530

    counting words

---
+++ wordcount.py
@@ -0,0 +1,9 @@
+sentence = "python is easy and python is powerful"
+words = sentence.split()
+
+frequency = {}
+
+for word in words:
+    frequency[word] = frequency.get(word, 0) + 1
+
+print(frequency)
