850a279178d8f54bbfb2beb9bae9ea1f747dcfe9
srushtee <srushteepatil1212@gmail.com>
2026-02-24 20:38:25 +0530

    decorator example with time

---
+++ decoratorexample.py
@@ -0,0 +1,16 @@
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Execution Time: {end - start:.5f} seconds")
+        return result
+    return wrapper
+
+@timer
+def slow_function():
+    time.sleep(2)
+
+slow_function()
